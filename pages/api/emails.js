c9a210eb83cf8139732c62f55e4ffc8850f801f9
Gorka <gorkavillara@gmail.com>
2021-11-19 14:48:54 +0100

    Nueva prueba email

--- pages/api/emails.js
+++ pages/api/emails.js
@@ -4,9 +4,10 @@ export default async function handler(req, res) {
     if (req.method === 'GET') {
     } else if (req.method === 'POST') {
         const mailconfig = {
+            from: "Sugerencias Newsletter Impact HUB",
             mailto: "gorkavillara@gmail.com",
-            subject: "Esto es una prueba",
-            text: "Hola Gorka esto es una prueba desde mailchimp"
+            subject: "¡Nueva sugerencia desde la newsletter!",
+            text: req.body.sugerencia
         }
         const resMail = await send(mailconfig);
         res.status(200).json(resMail);
